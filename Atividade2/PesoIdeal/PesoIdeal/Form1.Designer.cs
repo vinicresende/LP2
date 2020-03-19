45e3e02fcb3156bc544d87abab3fc982cb00f4e2
vinicresende <vinicresende@users.noreply.github.com>
2020-03-19 16:45:14 -0300

    1.0
    Versão Inicial

---
+++ Atividade2/PesoIdeal/PesoIdeal/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace PesoIdeal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.lblAltura = new System.Windows.Forms.Label();
+            this.txtAltura = new System.Windows.Forms.TextBox();
+            this.txtPeso = new System.Windows.Forms.TextBox();
+            this.lblPeso = new System.Windows.Forms.Label();
+            this.gbxSexo = new System.Windows.Forms.GroupBox();
+            this.rbtnFeminino = new System.Windows.Forms.RadioButton();
+            this.rbtnMasculino = new System.Windows.Forms.RadioButton();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.lblPesoIdeal = new System.Windows.Forms.Label();
+            this.lblSituacao = new System.Windows.Forms.Label();
+            this.lblResultadoPesoIdeal = new System.Windows.Forms.Label();
+            this.lblResultadoSituacao = new System.Windows.Forms.Label();
+            this.gbxSexo.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("BankGothic", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Location = new System.Drawing.Point(52, 27);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(398, 25);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "Calculadora do Peso Ideal";
+            this.lblTitulo.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // lblAltura
+            // 
+            this.lblAltura.AutoSize = true;
+            this.lblAltura.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAltura.Location = new System.Drawing.Point(31, 96);
+            this.lblAltura.Name = "lblAltura";
+            this.lblAltura.Size = new System.Drawing.Size(96, 18);
+            this.lblAltura.TabIndex = 1;
+            this.lblAltura.Text = "Altura (m):";
+            // 
+            // txtAltura
+            // 
+            this.txtAltura.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAltura.Location = new System.Drawing.Point(126, 93);
+            this.txtAltura.MaxLength = 4;
+            this.txtAltura.Name = "txtAltura";
+            this.txtAltura.Size = new System.Drawing.Size(102, 26);
+            this.txtAltura.TabIndex = 2;
+            this.txtAltura.Text = "1,72";
+            this.txtAltura.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtAltura_KeyPress);
+            // 
+            // txtPeso
+            // 
+            this.txtPeso.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPeso.Location = new System.Drawing.Point(126, 129);
+            this.txtPeso.MaxLength = 5;
+            this.txtPeso.Name = "txtPeso";
+            this.txtPeso.Size = new System.Drawing.Size(102, 26);
+            this.txtPeso.TabIndex = 4;
+            this.txtPeso.Text = "60,51";
+            // 
+            // lblPeso
+            // 
+            this.lblPeso.AutoSize = true;
+            this.lblPeso.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPeso.Location = new System.Drawing.Point(31, 132);
+            this.lblPeso.Name = "lblPeso";
+            this.lblPeso.Size = new System.Drawing.Size(96, 18);
+            this.lblPeso.TabIndex = 3;
+            this.lblPeso.Text = "Peso (kg) :";
+            // 
+            // gbxSexo
+            // 
+            this.gbxSexo.Controls.Add(this.rbtnFeminino);
+            this.gbxSexo.Controls.Add(this.rbtnMasculino);
+            this.gbxSexo.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gbxSexo.Location = new System.Drawing.Point(274, 76);
+            this.gbxSexo.Name = "gbxSexo";
+            this.gbxSexo.Size = new System.Drawing.Size(200, 92);
+            this.gbxSexo.TabIndex = 5;
+            this.gbxSexo.TabStop = false;
+            this.gbxSexo.Text = "Sexo:";
+            // 
+            // rbtnFeminino
+            // 
+            this.rbtnFeminino.AutoSize = true;
+            this.rbtnFeminino.Location = new System.Drawing.Point(28, 53);
+            this.rbtnFeminino.Name = "rbtnFeminino";
+            this.rbtnFeminino.Size = new System.Drawing.Size(90, 22);
+            this.rbtnFeminino.TabIndex = 1;
+            this.rbtnFeminino.Text = "Feminino";
+            this.rbtnFeminino.UseVisualStyleBackColor = true;
+            // 
+            // rbtnMasculino
+            // 
+            this.rbtnMasculino.AutoSize = true;
+            this.rbtnMasculino.Checked = true;
+            this.rbtnMasculino.Location = new System.Drawing.Point(28, 25);
+            this.rbtnMasculino.Name = "rbtnMasculino";
+            this.rbtnMasculino.Size = new System.Drawing.Size(98, 22);
+            this.rbtnMasculino.TabIndex = 0;
+            this.rbtnMasculino.TabStop = true;
+            this.rbtnMasculino.Text = "Masculino";
+            this.rbtnMasculino.UseVisualStyleBackColor = true;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.Location = new System.Drawing.Point(34, 196);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(440, 37);
+            this.btnCalcular.TabIndex = 6;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // lblPesoIdeal
+            // 
+            this.lblPesoIdeal.AutoSize = true;
+            this.lblPesoIdeal.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPesoIdeal.Location = new System.Drawing.Point(31, 253);
+            this.lblPesoIdeal.Name = "lblPesoIdeal";
+            this.lblPesoIdeal.Size = new System.Drawing.Size(96, 18);
+            this.lblPesoIdeal.TabIndex = 7;
+            this.lblPesoIdeal.Text = "Peso Ideal:";
+            // 
+            // lblSituacao
+            // 
+            this.lblSituacao.AutoSize = true;
+            this.lblSituacao.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSituacao.Location = new System.Drawing.Point(31, 289);
+            this.lblSituacao.Name = "lblSituacao";
+            this.lblSituacao.Size = new System.Drawing.Size(96, 18);
+            this.lblSituacao.TabIndex = 8;
+            this.lblSituacao.Text = "Situação  :";
+            // 
+            // lblResultadoPesoIdeal
+            // 
+            this.lblResultadoPesoIdeal.AutoSize = true;
+            this.lblResultadoPesoIdeal.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultadoPesoIdeal.Location = new System.Drawing.Point(133, 253);
+            this.lblResultadoPesoIdeal.Name = "lblResultadoPesoIdeal";
+            this.lblResultadoPesoIdeal.Size = new System.Drawing.Size(32, 18);
+            this.lblResultadoPesoIdeal.TabIndex = 9;
+            this.lblResultadoPesoIdeal.Text = "...";
+            // 
+            // lblResultadoSituacao
+            // 
+            this.lblResultadoSituacao.AutoSize = true;
+            this.lblResultadoSituacao.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultadoSituacao.Location = new System.Drawing.Point(133, 289);
+            this.lblResultadoSituacao.Name = "lblResultadoSituacao";
+            this.lblResultadoSituacao.Size = new System.Drawing.Size(32, 18);
+            this.lblResultadoSituacao.TabIndex = 10;
+            this.lblResultadoSituacao.Text = "...";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(513, 333);
+            this.Controls.Add(this.lblResultadoSituacao);
+            this.Controls.Add(this.lblResultadoPesoIdeal);
+            this.Controls.Add(this.lblSituacao);
+            this.Controls.Add(this.lblPesoIdeal);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.gbxSexo);
+            this.Controls.Add(this.txtPeso);
+            this.Controls.Add(this.lblPeso);
+            this.Controls.Add(this.txtAltura);
+            this.Controls.Add(this.lblAltura);
+            this.Controls.Add(this.lblTitulo);
+            this.Font = new System.Drawing.Font("Calibri", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.gbxSexo.ResumeLayout(false);
+            this.gbxSexo.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Label lblAltura;
+        private System.Windows.Forms.TextBox txtAltura;
+        private System.Windows.Forms.TextBox txtPeso;
+        private System.Windows.Forms.Label lblPeso;
+        private System.Windows.Forms.GroupBox gbxSexo;
+        private System.Windows.Forms.RadioButton rbtnFeminino;
+        private System.Windows.Forms.RadioButton rbtnMasculino;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label lblPesoIdeal;
+        private System.Windows.Forms.Label lblSituacao;
+        private System.Windows.Forms.Label lblResultadoPesoIdeal;
+        private System.Windows.Forms.Label lblResultadoSituacao;
+    }
+}
+
---
+++ Atividade2/PesoIdeal/PesoIdeal/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PesoIdeal
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            double altura, pesoAtual, pesoIdeal; //Variáveis locais
+            if(rbtnMasculino.Checked) //BOTÃO MASCULINO ESTÁ CHECKADO
+            {
+                
+                if (double.TryParse(txtAltura.Text, out altura) && double.TryParse(txtPeso.Text, out pesoAtual)) //TENTATIVA DE CONVERSÃO PARA NÚMERO
+                {
+                    pesoIdeal = 72.7 * altura - 58;
+                    pesoIdeal = Math.Round(pesoIdeal, 1);
+                    lblResultadoPesoIdeal.Text = pesoIdeal.ToString();
+
+                    if (pesoAtual < pesoIdeal)
+                    {
+                        lblResultadoSituacao.Text = "Abaixo do Peso Ideal - Ganhe massa!!";
+                        lblResultadoSituacao.ForeColor = Color.FromArgb(255, 0, 0);
+                    }
+                    else if (pesoAtual > pesoIdeal)
+                    {
+                        lblResultadoSituacao.Text = "Acima do Peso Ideal - Perca massa!!";
+                        lblResultadoSituacao.ForeColor = Color.FromArgb(255, 0, 0);
+                    }
+                    else
+                    {
+                        lblResultadoSituacao.Text = "Você está no Peso Ideal - Parabéns!!";
+                        lblResultadoSituacao.ForeColor = Color.FromArgb(0, 0, 255);
+                    }
+                }
+                else //NÃO É NÚMERICA A ENTRADA
+                    MessageBox.Show("Insira entradas válidas para Altura e Peso", "Erro", MessageBoxButtons.YesNo, MessageBoxIcon.Error);
+            }
+            else //BOTÃO MASCULINO NÃO CHECKADO
+            {
+                if (double.TryParse(txtAltura.Text, out altura) && double.TryParse(txtPeso.Text, out pesoAtual)) //TENTATIVA DE CONVERSÃO PARA NÚMERO
+                {
+                    pesoIdeal = 62.1 * altura - 44.7;
+                    pesoIdeal = Math.Round(pesoIdeal, 1);
+                    lblResultadoPesoIdeal.Text = pesoIdeal.ToString();
+
+                    if (pesoAtual < pesoIdeal)
+                    {
+                        lblResultadoSituacao.Text = "Abaixo do Peso Ideal - Ganhe massa!!";
+                        lblResultadoSituacao.ForeColor = Color.FromArgb(255, 0, 0);
+                    }
+                    else if (pesoAtual > pesoIdeal)
+                    {
+                        lblResultadoSituacao.Text = "Acima do Peso Ideal - Perca massa!!";
+                        lblResultadoSituacao.ForeColor = Color.FromArgb(255, 0, 0);
+                    }
+                    else
+                    {
+                        lblResultadoSituacao.Text = "Você está no Peso Ideal - Parabéns!!";
+                        lblResultadoSituacao.ForeColor = Color.FromArgb(0, 0, 255);
+                    }
+                }
+                else //NÃO É NÚMERICA A ENTRADA
+                    MessageBox.Show("Insira entradas válidas para Altura e Peso", "Erro", MessageBoxButtons.YesNo, MessageBoxIcon.Error);
+            }
+        }
+
+        private void txtAltura_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!(char.IsDigit(e.KeyChar) || char.IsControl(e.KeyChar) || e.KeyChar==',' || e.KeyChar == '.'))
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
