0e9f3e03806edeac94bf86e1533944d3639f2662
vinicresende <vinicresende@users.noreply.github.com>
2020-06-19 17:55:57 -0300

    v1.0
    Versão Inicial

---
+++ Atividade7/Atividade 7/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Atividade_7
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExercicio6 = new System.Windows.Forms.Button();
+            this.btnExercicio5 = new System.Windows.Forms.Button();
+            this.btnExercicio4 = new System.Windows.Forms.Button();
+            this.btnExercicio3 = new System.Windows.Forms.Button();
+            this.btnExercicio2 = new System.Windows.Forms.Button();
+            this.btnExercicio1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnExercicio6
+            // 
+            this.btnExercicio6.Location = new System.Drawing.Point(396, 439);
+            this.btnExercicio6.Margin = new System.Windows.Forms.Padding(4);
+            this.btnExercicio6.Name = "btnExercicio6";
+            this.btnExercicio6.Size = new System.Drawing.Size(325, 164);
+            this.btnExercicio6.TabIndex = 11;
+            this.btnExercicio6.Text = "Exercício 6: Media das Notas";
+            this.btnExercicio6.UseVisualStyleBackColor = true;
+            this.btnExercicio6.Click += new System.EventHandler(this.btnExercicio6_Click);
+            // 
+            // btnExercicio5
+            // 
+            this.btnExercicio5.Location = new System.Drawing.Point(43, 439);
+            this.btnExercicio5.Margin = new System.Windows.Forms.Padding(4);
+            this.btnExercicio5.Name = "btnExercicio5";
+            this.btnExercicio5.Size = new System.Drawing.Size(325, 164);
+            this.btnExercicio5.TabIndex = 10;
+            this.btnExercicio5.Text = "Exercício 5: Alunos com ArrayList";
+            this.btnExercicio5.UseVisualStyleBackColor = true;
+            this.btnExercicio5.Click += new System.EventHandler(this.btnExercicio5_Click);
+            // 
+            // btnExercicio4
+            // 
+            this.btnExercicio4.Location = new System.Drawing.Point(396, 254);
+            this.btnExercicio4.Margin = new System.Windows.Forms.Padding(4);
+            this.btnExercicio4.Name = "btnExercicio4";
+            this.btnExercicio4.Size = new System.Drawing.Size(325, 164);
+            this.btnExercicio4.TabIndex = 9;
+            this.btnExercicio4.Text = "Eercício 4: Contar Letras Alunos";
+            this.btnExercicio4.UseVisualStyleBackColor = true;
+            this.btnExercicio4.Click += new System.EventHandler(this.btnExercicio4_Click);
+            // 
+            // btnExercicio3
+            // 
+            this.btnExercicio3.Location = new System.Drawing.Point(43, 254);
+            this.btnExercicio3.Margin = new System.Windows.Forms.Padding(4);
+            this.btnExercicio3.Name = "btnExercicio3";
+            this.btnExercicio3.Size = new System.Drawing.Size(325, 164);
+            this.btnExercicio3.TabIndex = 8;
+            this.btnExercicio3.Text = "Exercicio 3: Calculo Faturamento Armazem";
+            this.btnExercicio3.UseVisualStyleBackColor = true;
+            this.btnExercicio3.Click += new System.EventHandler(this.btnExercicio3_Click);
+            // 
+            // btnExercicio2
+            // 
+            this.btnExercicio2.Location = new System.Drawing.Point(396, 69);
+            this.btnExercicio2.Margin = new System.Windows.Forms.Padding(4);
+            this.btnExercicio2.Name = "btnExercicio2";
+            this.btnExercicio2.Size = new System.Drawing.Size(325, 164);
+            this.btnExercicio2.TabIndex = 7;
+            this.btnExercicio2.Text = "Exercicio 2: Ler 20 Numeros e inverter usando reverse";
+            this.btnExercicio2.UseVisualStyleBackColor = true;
+            this.btnExercicio2.Click += new System.EventHandler(this.btnExercicio2_Click);
+            // 
+            // btnExercicio1
+            // 
+            this.btnExercicio1.Location = new System.Drawing.Point(43, 69);
+            this.btnExercicio1.Margin = new System.Windows.Forms.Padding(4);
+            this.btnExercicio1.Name = "btnExercicio1";
+            this.btnExercicio1.Size = new System.Drawing.Size(325, 164);
+            this.btnExercicio1.TabIndex = 6;
+            this.btnExercicio1.Text = "Exercício 1: Ler 20 Numeros e Inverter";
+            this.btnExercicio1.UseVisualStyleBackColor = true;
+            this.btnExercicio1.Click += new System.EventHandler(this.btnExercicio1_Click);
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(768, 660);
+            this.Controls.Add(this.btnExercicio6);
+            this.Controls.Add(this.btnExercicio5);
+            this.Controls.Add(this.btnExercicio4);
+            this.Controls.Add(this.btnExercicio3);
+            this.Controls.Add(this.btnExercicio2);
+            this.Controls.Add(this.btnExercicio1);
+            this.Name = "frmPrincipal";
+            this.Text = "Atividade 7";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExercicio6;
+        private System.Windows.Forms.Button btnExercicio5;
+        private System.Windows.Forms.Button btnExercicio4;
+        private System.Windows.Forms.Button btnExercicio3;
+        private System.Windows.Forms.Button btnExercicio2;
+        private System.Windows.Forms.Button btnExercicio1;
+    }
+}
+
---
+++ Atividade7/Atividade 7/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+using System.Collections;
+
+namespace Atividade_7
+{
+    public partial class frmPrincipal : Form
+    {
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExercicio1_Click(object sender, EventArgs e)
+        {
+            int[] Vetor = new int[20];
+            string auxiliar = "";
+            string valor = "";
+
+            for (var x = 0; x < 20; x++)
+            {
+                valor = Interaction.InputBox("Digite um Valor na posição " + (x + 1), "Entrada de Dados");
+
+                if (int.TryParse(valor, out Vetor[x]))
+                {
+                    //auxiliar = auxiliar + "\n" + Vetor[x].ToString(); concatena no final e não inverte 
+                    auxiliar = Vetor[x].ToString() + "\n" + auxiliar; //concatenando no começo e inverte
+                }
+                else
+                {
+                    MessageBox.Show("Número Invalido!!");
+                    x--; //decrementa para repedir a mesma entrada quando voltar no for.
+                }
+
+            }
+            MessageBox.Show(auxiliar);
+        }
+
+        private void btnExercicio2_Click(object sender, EventArgs e)
+        {
+            int[] Vetor = new int[20];
+            string auxiliar = "";
+            string valor = "";
+
+            for (var x = 0; x < 20; x++)
+            {
+                valor = Interaction.InputBox("Digite um Valor na posição " + (x + 1), "Entrada de Dados");
+                if (!int.TryParse(valor, out Vetor[x]))
+                {
+                    MessageBox.Show("Número Invalido!!");
+                    x--; //decrementa para repedir a mesma entrada quando voltar no for.
+                }
+
+            }
+            Array.Reverse(Vetor);
+            for (var x = 19; x <= 0; x--)
+                auxiliar = auxiliar + "\n" + Vetor[x];
+            /* Um jeito de Fazer sem o reverse
+            for (var x=19;x<=0;x--)
+                auxiliar = auxiliar + "\n" + Vetor[x].ToString()
+            */
+            MessageBox.Show(auxiliar);
+        }
+
+        private void btnExercicio3_Click(object sender, EventArgs e)
+        {
+            double[] Preco = new double[10];
+            double[] Quantidade = new double[10];
+            double faturamento = 0;
+            string valor = "";
+
+            for (var x = 0; x < 10; x++)
+            {
+                valor = Interaction.InputBox("Quantidade da Mercadoria " + (x + 1) + ":", "Entrada de Dados");
+                if (double.TryParse(valor, out Quantidade[x]))
+                {
+                    while (true)
+                    {
+                        valor = Interaction.InputBox("Valor da Mercadoria " + (x + 1) + ":", "Entrada de Dados");
+                        if (double.TryParse(valor, out Preco[x]))
+                            break;
+                        else
+                            MessageBox.Show("Digite o Valor da Mercadoria " + (x + 1) + " corretamente!!!");
+                    }
+                    faturamento += Quantidade[x] * Preco[x];
+                }
+                else
+                {
+                    MessageBox.Show("Digite a Quantidade da Mercadoria " + (x + 1) + " corretamente!!!");
+                    x--;
+                }
+            }
+            MessageBox.Show("Faturamento = R$" + faturamento.ToString("N2"));
+        }
+
+        private void btnExercicio4_Click(object sender, EventArgs e)
+        {
+            string[] Alunos = { "Viviane", "André", "Hélio", "Denise", "Junior", "Leonardo", "Jose", "Nelma", "Tobby" };
+            Int32 I, Total = 0;
+            Int32 N = Alunos.Length;
+            for (I = 0; I < N - 1; I++)
+            {
+                Total += Alunos[I].Length;
+            }
+            MessageBox.Show(Total.ToString());
+        }
+
+        private void btnExercicio5_Click(object sender, EventArgs e)
+        {
+            string[] Alunos = { "Ana", "André", "Débora", "Fátima", "João", "Janete", "Otávio", "Marcelo", "Pedro", "Thais" };
+            string auxiliar = "";
+            ArrayList AlunosList = new ArrayList();
+            AlunosList.AddRange(Alunos);
+            AlunosList.Remove("Otávio");
+            foreach (String str in AlunosList)
+                auxiliar += str + "\n";
+            MessageBox.Show(auxiliar);
+        }
+
+        private void btnExercicio6_Click(object sender, EventArgs e)
+        {
+            double[,] notas = new double[20, 3];
+            double medias;
+            int i, j;
+            string valor, mediaFinal = "";
+            for (i = 0; i < 20; i++)
+            {
+                medias = 0;
+                for (j = 0; j < 3; j++)
+                {
+                    valor = Interaction.InputBox("Nota " + (j + 1) + " do aluno " + (i + 1) + ":", "Entrada das Notas");
+                    if (double.TryParse(valor, out notas[i, j]))
+                    {
+                        medias += notas[i, j];
+                    }
+                    else
+                    {
+                        MessageBox.Show("Digite valores válidos para as notas!!!");
+                        j--;
+                    }
+                }
+                medias /= 3;
+                mediaFinal += "Aluno "+ (i+1) + " : média: " + medias.ToString("N1") + "\n";
+            }
+            MessageBox.Show(mediaFinal);
+        }
+    }
+}
